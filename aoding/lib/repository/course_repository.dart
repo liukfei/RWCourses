6f3c00a0c2bbb2d255cbdbe9dd0dd97ae8ec90ce
edwardliu <liukfei@gmail.com>
2021-03-25 21:17:13 +0800

    Add http support;

--- aoding/lib/repository/course_repository.dart
+++ aoding/lib/repository/course_repository.dart
@@ -4,14 +4,14 @@ import 'repository.dart';
 
 class CourseRepository implements Repository {
 
-  final String dataURL = "";
+  final String dataURL = "https://api.raywenderlich.com/api/contents?filter[content_types][]=collection";
 
   @override
   Future<List<Course>> getCourses(int domainFilter) async {
     var courses = <Course>[];
     var url = dataURL;
     if (domainFilter != Constants.allFilter) {
-      url += "";
+      url += "&filter[domain_ids][]=$domainFilter";
     }
 
     return courses;
